6e300da1653c4420c58c97159eccbc0a92e9a111
jjlee <cooco119@gmail.com>
2021-02-13 15:08:56 +0900

    Implement helloworld page

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,4 +1,4 @@
-"""app URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
 ]
